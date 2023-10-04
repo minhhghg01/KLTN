753b9c2a5e75413d8f157032fa4cc4ee4d37baa0
minhhghg01 <20020060@vnu.edu.vn>
2023-10-05 01:10:34 +0700

    tkb

--- fe/src/component/home.jsx
+++ fe/src/component/home.jsx
@@ -11,19 +11,20 @@ const items1 = ['1', '2', '3'].map((key) => ({
   key,
   label: `nav ${key}`,
 }));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+const items2 = ["TKB", "bds", "sas"].map((item, index) => {
   const key = String(index + 1);
   return {
     key: `/sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
+    icon: React.createElement(item),
+    label: ` ${item}`,
+    children: ["TKB", "điểm danh", "học phí" ].map((child) =>{
+      if(item==="TKB"){
       return {
-        key: `/sub${subKey}`,
-        label: `option${subKey}`,
-      };
-    }),
+        key: `/${child}`,
+        label: ` ${child}`,
+      }}
+    })
+    
   };
 });
 const App = () => {
